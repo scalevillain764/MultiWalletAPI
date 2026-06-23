79fff2427a5621c7bae31939a3036d4ac1420634
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-06-23 13:00:25 +0300

    feat: add Constructorn in WalletResonseDTO, add Wallet Service

--- MultiWallet API/DTO/WalletDTO/WalletResponseDTO.cs
+++ MultiWallet API/DTO/WalletDTO/WalletResponseDTO.cs
@@ -1,4 +1,5 @@
-﻿namespace _wallet_response_dto
+﻿using _wallet;
+namespace _wallet_response_dto
 {
     public class WalletResponseDTO
     {
@@ -8,5 +9,14 @@
         public string Currency { get; set; }
         public decimal Balance { get; set; }
         public DateTime CreatedAt { get; set; }
+        public WalletResponseDTO(Wallet wallet)
+        {
+            Name = wallet.Name;
+            Id = wallet.Id.ToString();
+            UserId = wallet.UserId.ToString();
+            Currency = wallet._Currency.ToString();
+            Balance = wallet.Balance;
+            CreatedAt = wallet.CreatedAt;
+        }
     }
 }
---
+++ MultiWallet API/Services/WalletService.cs
@@ -0,0 +1,31 @@
+﻿using _wallet;
+using _interfaces;
+using _context;
+using _result;
+
+using _wallet_creation_dto;
+using _wallet_response_dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace _wallet_service
+{
+    public class WalletService : IWalletService
+    {
+        private readonly AppDbContext _context;
+        public WalletService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Result<WalletResponseDTO>> AddWalletAsync(Ulid UserId, WalletCreationDTO walletCreationDTO)
+        {
+            bool currencyExists = await _context.Wallets
+                .Where(x => x.UserId == UserId)
+                .AnyAsync(x => (int)x._Currency == walletCreationDTO.CurrencyEnum);
+
+            if (currencyExists)
+                return Result<WalletResponseDTO>.Error("Счет с такой валютой уже существует");
+
+            var newWallet = new Wallet(walletCreationDTO.Name, UserId, (Wallet.Currency)walletCreationDTO.CurrencyEnum);
+        }
+    }
+}
