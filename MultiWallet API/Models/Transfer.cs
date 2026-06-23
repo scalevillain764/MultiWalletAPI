ea3f5429634539708ebe7b62cc71db27b2cbdc80
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-06-23 13:40:42 +0300

    feat: add TransferEntity

---
+++ MultiWallet API/Models/Transfer.cs
@@ -0,0 +1,20 @@
+﻿using _user;
+namespace _transfer
+{
+    public class Transfer
+    {
+        public Ulid FromId { get; set; }
+        public User FromUser { get; set; } = null;
+        public Ulid ToId { get; set; }
+        public User ToUser { get; set; } = null;
+        public decimal Amount { get; set; }
+        public Transfer(Ulid fromId, User fromUser, Ulid toId, User toUser, decimal amount)
+        {
+            FromId = fromId;
+            FromUser = fromUser;
+            ToId = toId;
+            ToUser = toUser;
+            Amount = amount;
+        }
+    }
+}
