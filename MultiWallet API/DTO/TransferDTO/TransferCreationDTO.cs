0c51eceadb223e5f157523d1127522c3b149f436
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-06-23 23:25:23 +0300

    feat: add TransferService, fix: fix TransferService

--- MultiWallet API/DTO/TransferDTO/TransferCreationDTO.cs
+++ MultiWallet API/DTO/TransferDTO/TransferCreationDTO.cs
@@ -4,10 +4,14 @@
     {
         public string FromWalletId { get; set; }
         public string ToWalletId { get; set; }
-        public TransferCreationDTO(string fromWalletId, string toWalletId)
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+        public TransferCreationDTO(string fromWalletId, string toWalletId, decimal amount, string? description)
         {
+            Amount = amount;
             FromWalletId = fromWalletId;
             ToWalletId = toWalletId;
+            Description = description;
         }
     }
 }
--- MultiWallet API/Services/TransferService.cs
+++ MultiWallet API/Services/TransferService.cs
@@ -2,6 +2,10 @@
 using _transfer_creation_dto;
 using _interfaces;
 using _context;
+using _result;
+using _transaction;
+using Microsoft.EntityFrameworkCore;
+using _transfer;
 namespace _transfer_service
 {
     public class TransferService : ITransferService
@@ -11,9 +15,51 @@ namespace _transfer_service
         {
             _context = context; 
         }
-        public async Task<Results<TransferResponseDTO>> MakeTransfer(Ulid UserId, TransferCreationDTO transferCreationDTO)
+
+        public async Task<Result<TransferResponseDTO>> MakeTransfer(Ulid UserId, TransferCreationDTO transferCreationDTO)
         {
-            var fromWallet = await _context.
+            if (!Ulid.TryParse(transferCreationDTO.FromWalletId, out var fromWalletId))
+                return Result<TransferResponseDTO>.Error("Неверный № счета отправителя");
+
+            var fromWallet = await _context.Wallets
+                .FirstOrDefaultAsync(x => x.Id == fromWalletId);
+
+            if (fromWallet == null)
+                return Result<TransferResponseDTO>.Error("Счет отправителя не найден");
+
+            if (!Ulid.TryParse(transferCreationDTO.ToWalletId, out var toWalletId))
+                return Result<TransferResponseDTO>.Error("Неверный № счета получателя");
+
+            var toWallet = await _context.Wallets
+                .FirstOrDefaultAsync(x => x.Id == toWalletId);
+
+            if (toWallet == null)
+                return Result<TransferResponseDTO>.Error("Счет получателя не найден");
+
+            if (transferCreationDTO.Amount > fromWallet.Balance)
+                return Result<TransferResponseDTO>.Error("Недостаточно средств");
+
+            fromWallet.Balance -= transferCreationDTO.Amount;
+            toWallet.Balance += transferCreationDTO.Amount;
+
+            var fromWalletTransaction = new Transaction(UserId, fromWalletId, transferCreationDTO.Amount, Transaction.TransactionType.Transfer, transferCreationDTO.Description);
+            var toWalletTransaction = new Transaction(toWallet.UserId, toWalletId, transferCreationDTO.Amount, Transaction.TransactionType.Transfer, transferCreationDTO.Description);
+            var newTransfer = new Transfer(fromWalletId, toWalletId, transferCreationDTO.Amount);
+
+            _context.Transactions.Add(fromWalletTransaction);
+            _context.Transactions.Add(toWalletTransaction);
+
+            try  
+            {
+                await _context.SaveChangesAsync();
+                return Result<TransferResponseDTO>.Success(new TransferResponseDTO(newTransfer));
+            }
+            catch (Exception ex)
+            {
+                return Result<TransferResponseDTO>.Error("Что-то пошло не так");
+            }
+
+            // потом доделать курсы валют с апи!
         }
     }
 }
