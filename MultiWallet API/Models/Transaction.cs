344c0cf33fc020f114ce4618aebf14955556b34e
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-06-23 13:27:43 +0300

    feat: add transaction entity

---
+++ MultiWallet API/Models/Transaction.cs
@@ -0,0 +1,35 @@
+﻿using _user;
+using _wallet;
+using _interfaces;
+
+using Microsoft.AspNetCore.SignalR;
+namespace _transaction
+{
+    public class Transaction : IEntity
+    {
+        public enum TransactionType { Income = 1, Expense = 2, Transfer = 3, Deposit = 4 /*через ПС*/}
+        public enum TransactionStatus { Pending = 1, Completed = 2, Failed = 3, Cancelled = 4} 
+
+        public Ulid Id { get; set; }
+        public Ulid UserId { get; set; }
+        public User User { get; set; } = null;
+        public Ulid WalletId { get; set; }
+        public Wallet Wallet { get; set; } = null;
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+        public TransactionStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? Description { get; set; }
+        public Transaction(Ulid userId, Ulid walletId, decimal amount, TransactionType type, TransactionStatus status, string? description)
+        {
+            Id = Ulid.NewUlid();
+            UserId = userId;
+            WalletId = walletId;
+            Amount = amount;
+            Type = type;
+            Status = status;
+            CreatedAt = DateTime.UtcNow;
+            Description = description;
+        }
+    }
+}
